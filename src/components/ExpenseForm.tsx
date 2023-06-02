b8ea0ec2f5e136e7dd4dce213e3e5a97bc6bd582
penevl <penevl@protonmail.com>
2023-06-02 16:54:40 +0300

    Added a form to the ExpenseForm

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -17,9 +17,15 @@ import {
   DrawerCloseButton,
   useDisclosure,
 } from "@chakra-ui/react";
+import { FormEvent } from "react";
 
 function ExpenseForm() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onClose();
+  };
 
   return (
     <>
@@ -31,55 +37,58 @@ function ExpenseForm() {
         <DrawerContent>
           <DrawerCloseButton />
           <DrawerHeader borderBottomWidth="1px">Add an expense</DrawerHeader>
-
-          <DrawerBody>
-            <Stack spacing="24px">
-              <Box>
-                <FormLabel htmlFor="expense">Expense name</FormLabel>
-                <Input
-                  id="expense"
-                  placeholder="Please enter an expense name"
-                  type="text"
-                />
-              </Box>
-
-              <Box>
-                <FormLabel htmlFor="expense">Expense ammount</FormLabel>
-                <InputGroup>
+          <form onSubmit={handleSubmit}>
+            <DrawerBody>
+              <Stack spacing="24px">
+                <Box>
+                  <FormLabel htmlFor="expense">Expense name</FormLabel>
                   <Input
                     id="expense"
-                    placeholder="Please enter an expense ammount"
-                    type="number"
+                    placeholder="Please enter an expense name"
+                    type="text"
                   />
-                  <InputRightAddon>$</InputRightAddon>
-                </InputGroup>
-              </Box>
+                </Box>
 
-              <Box>
-                <FormLabel htmlFor="type">Select expense type</FormLabel>
-                <Select id="type" defaultValue="groceries">
-                  <option value="entertainment">Entertainment</option>
-                  <option value="utilities">Utilities</option>
-                  <option value="groceries">Groceries</option>
-                  <option value="Other">Other</option>
-                </Select>
-              </Box>
+                <Box>
+                  <FormLabel htmlFor="expense">Expense ammount</FormLabel>
+                  <InputGroup>
+                    <Input
+                      id="expense"
+                      placeholder="Please enter an expense ammount"
+                      type="number"
+                    />
+                    <InputRightAddon>$</InputRightAddon>
+                  </InputGroup>
+                </Box>
 
-              <Box>
-                <FormLabel htmlFor="description">
-                  Description(Optional)
-                </FormLabel>
-                <Textarea id="description" />
-              </Box>
-            </Stack>
-          </DrawerBody>
+                <Box>
+                  <FormLabel htmlFor="type">Select expense type</FormLabel>
+                  <Select id="type" defaultValue="groceries">
+                    <option value="entertainment">Entertainment</option>
+                    <option value="utilities">Utilities</option>
+                    <option value="groceries">Groceries</option>
+                    <option value="Other">Other</option>
+                  </Select>
+                </Box>
 
-          <DrawerFooter borderTopWidth="1px">
-            <Button variant="outline" mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme="blue">Submit</Button>
-          </DrawerFooter>
+                <Box>
+                  <FormLabel htmlFor="description">
+                    Description(Optional)
+                  </FormLabel>
+                  <Textarea id="description" />
+                </Box>
+              </Stack>
+            </DrawerBody>
+
+            <DrawerFooter borderTopWidth="1px">
+              <Button variant="outline" mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="blue" type="submit">
+                Submit
+              </Button>
+            </DrawerFooter>
+          </form>
         </DrawerContent>
       </Drawer>
     </>
